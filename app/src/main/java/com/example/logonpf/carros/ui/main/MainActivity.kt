8a8b7cacb1b80c62ac2a9d3d16a40ac0d9809ea4
logonrm <logonrm@fiap.com.br>
2018-03-10 15:03:54 -0300

    UIs novocarro, main, listacarros, splash

--- app/src/main/java/com/example/logonpf/carros/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/logonpf/carros/ui/main/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.example.logonpf.carros
+package com.example.logonpf.carros.ui.main
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import com.example.logonpf.carros.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/logonpf/carros/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/logonpf/carros/ui/splash/SplashActivity.kt
@@ -1,10 +1,12 @@
-package com.example.logonpf.carros
+package com.example.logonpf.carros.ui.splash
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
+import com.example.logonpf.carros.R
+import com.example.logonpf.carros.ui.main.MainActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
